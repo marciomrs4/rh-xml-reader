8f2a95f8d2efe8b29007e6cdd046f15fb3975c8f
Márcio Ramos <marciomrs4@hotmail.com>
2018-09-14 13:10:29 -0300

    Fix security access

--- src/RH/XMLReaderBundle/Controller/DefaultController.php
+++ src/RH/XMLReaderBundle/Controller/DefaultController.php
@@ -3,11 +3,26 @@
 namespace RH\XMLReaderBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
 
 class DefaultController extends Controller
 {
+    /**
+     * @Route("/",name="index")
+     */
     public function indexAction()
+    {
+        return $this->redirectToRoute('home');
+    }
+
+    /**
+     * @Route("/home",name="home")
+     */
+    public function homeAction()
     {
         return $this->render('RHXMLReaderBundle:Default:index.html.twig');
     }
+
+
 }
